ac7643d24ad2a4af710d9ab6c70a0db67f574f3c
Leopotam <leopotam@gmail.com>
2016-02-14 19:30:22 +0300

    [+] LeopotamGroup.Scripting examples.

---
+++ Assets/LeopotamGroup.Examples/Scripting/MyScriptManager.cs
@@ -0,0 +1,39 @@
+﻿using LeopotamGroup.Scripting;
+using UnityEngine;
+
+namespace LeopotamGroup.Examples.ScriptingTest {
+    class MyScriptManager : ScriptManagerBase<MyScriptManager> {
+        protected override void OnAttachHostFunctions (ScriptVM vm) {
+            base.OnAttachHostFunctions (vm);
+            // Registering our custom methods for access from scripts.
+            vm.RegisterHostFunction ("test_sqrt", OnSqrt);
+            vm.RegisterHostFunction ("test_echo", OnTest);
+        }
+
+        protected override void OnRuntimeError (string errMsg) {
+            Debug.LogError ("Script error: " + errMsg);
+            base.OnRuntimeError (errMsg);
+        }
+
+        ScriptVar OnSqrt (ScriptVM vm) {
+            var count = vm.GetParamsCount ();
+            var v = vm.GetParamByID (0);
+            if (count < 1 || !v.IsNumber) {
+                vm.SetRuntimeError ("(nValue) parameter required");
+                return new ScriptVar ();
+            }
+            return new ScriptVar (Mathf.Sqrt (v.AsNumber));
+        }
+
+        ScriptVar OnTest (ScriptVM vm) {
+            var count = vm.GetParamsCount ();
+            if (count != 1) {
+                vm.SetRuntimeError ("test_echo function requires only one parameter");
+                return new ScriptVar ();
+            }
+            var v = vm.GetParamByID (0);
+            Debug.Log ("OnTest callback called with parameter: " + v.AsString);
+            return v;
+        }
+    }
+}
---
+++ Assets/LeopotamGroup.Examples/Scripting/ScriptingTest.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using LeopotamGroup.Scripting;
+
+namespace LeopotamGroup.Examples.ScriptingTest {
+    public class ScriptingTest : MonoBehaviour {
+        const string ScriptName = "Scripts/TestScript";
+
+        const string MainFunctionName = "main";
+
+        void Start () {
+            var scriptSource = Resources.Load <TextAsset> (ScriptName).text;
+            var err = MyScriptManager.Instance.LoadSource (scriptSource);
+            if (err != null) {
+                // We have error here.
+                Debug.LogWarning (err);
+                return;
+            }
+            // Code was loaded and parsed correctly, we ready to call functions
+
+            ScriptVar retVal;
+            err = MyScriptManager.Instance.CallFunction (MainFunctionName, out retVal, new ScriptVar (1), new ScriptVar ("str"));
+            if (err != null) {
+                // We have error here.
+                Debug.LogWarning (err);
+                return;
+            }
+        }
+    }
+}
