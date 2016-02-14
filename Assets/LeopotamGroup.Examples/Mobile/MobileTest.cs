e3c7187ad474d7b535229056632fddbc5adaefb0
Leopotam <leopotam@gmail.com>
2016-02-14 14:37:49 +0300

    [+] LeopotamGroup.Mobile examples.

---
+++ Assets/LeopotamGroup.Examples/Mobile/MobileTest.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace LeopotamGroup.Examples.MobileTest {
+    public class MobileTest : MonoBehaviour {
+        void OnGUI () {
+            GUILayout.Label ("On android if you will press 'back' button - app will be closed.\n" +
+            "At editor / standalone app you can press 'Escape' key with same result.");
+        }
+
+        #region UI callbacks
+
+        public void OnBackPressed () {
+            // This event was subscribed from inspector of BackButtonBehaviour.
+            Debug.Log ("Back pressed");
+            Application.Quit ();
+        }
+
+        #endregion
+    }
+}
