65576e00dae60f15157eff1d967ac74197a4c6ac
Leopotam <leopotam@gmail.com>
2016-03-03 01:43:32 +0300

    [+] LeopotamGroup.Protection: Examples.

---
+++ Assets/LeopotamGroup.Examples/Protection/ProtectionTest.cs
@@ -0,0 +1,36 @@
+﻿using LeopotamGroup.Protection;
+using UnityEngine;
+
+namespace LeopotamGroup.Examples.ProtectionTest {
+    public class ProtectionTest : MonoBehaviour {
+        void Start () {
+            IntTest ();
+            LongTest ();
+            FloatTest ();
+        }
+
+        void IntTest () {
+            Debug.Log (">>>>> Test protection of int >>>>>");
+            int testValue = 12345678;
+            ProtInt protValue = testValue;
+            Debug.LogFormat ("{0} encrypted to {1}", testValue, protValue.EncryptedValue);
+            Debug.LogFormat ("{0} decrypted to {1}", protValue.EncryptedValue, (int) protValue);
+        }
+
+        void LongTest () {
+            Debug.Log (">>>>> Test protection of long >>>>>");
+            long testValue = 12345678L;
+            ProtLong protValue = testValue;
+            Debug.LogFormat ("{0} encrypted to {1}", testValue, protValue.EncryptedValue);
+            Debug.LogFormat ("{0} decrypted to {1}", protValue.EncryptedValue, (long) protValue);
+        }
+
+        void FloatTest () {
+            Debug.Log (">>>>> Test protection of float >>>>>");
+            float testValue = 1234.5678f;
+            ProtFloat protValue = testValue;
+            Debug.LogFormat ("{0} encrypted to {1}", testValue, protValue.EncryptedValue);
+            Debug.LogFormat ("{0} decrypted to {1}", protValue.EncryptedValue, (float) protValue);
+        }
+    }
+}
